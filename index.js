24537060d1a8cb7b73b58024eae1bc597a6bc12b
calvinlee326 <calvinlee326@gmail.com>
2026-03-13 13:04:52 -0700

    Add icons, update styles, and implement lead rendering in Chrome extension

--- index.js
+++ index.js
@@ -5,14 +5,22 @@ const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value);
-    inputEl.value = "";
-    console.log(myLeads);
+    inputEl.value = ""; // Clear the input field
+    renderLeads();
+});
 
+function renderLeads() {
     let listItems = "";
-    for (let i=0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>";
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+        <li>
+            <a target='_blank' href='${myLeads[i]}'>
+                ${myLeads[i]}
+            </a>
+        </li> 
+        `
     }
     ulEl.innerHTML = listItems;
-});
+}
 
 
