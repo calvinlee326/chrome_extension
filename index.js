1b81c8d49c0ae86d964ed759fbc36b0599a9b623
calvinlee326 <calvinlee326@gmail.com>
2026-03-13 16:42:57 -0700

    Enhance button functionality and styling; add delete feature for leads

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 let myLeads = [];
+
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    renderLeads();
+}
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear();
+    myLeads = [];
+    renderLeads();
+});
+
+
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value);
     inputEl.value = ""; // Clear the input field
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+
     renderLeads();
 });
+
+
 
 function renderLeads() {
     let listItems = "";
