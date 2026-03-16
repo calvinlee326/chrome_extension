d404dfe95cb5fffe27e14df26f46982c252dbeb3
calvinlee326 <calvinlee326@gmail.com>
2026-03-16 16:40:52 -0700

    Add 'SAVE TAB' button functionality to store current tab URL; update button labels and manifest permissions for tab access

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
+const TabBtn = document.getElementById("tab-btn")
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
     render(myLeads);
 }
+
+
+TabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        render(myLeads);
+    });
+}) ;
 
 deleteBtn.addEventListener("dblclick", function(){
     localStorage.clear();
